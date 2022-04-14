d2d03df8b3fa7b295003c6558e8e8adaa2a4d077
Gibigabcsusz <gbr.szilagyi@gmail.com>
2022-04-14 21:56:55 +0200

    láncolt listás bohóckodás, de nincs messze a vége

--- primszuro.c
+++ primszuro.c
@@ -2,23 +2,79 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-int main(){
-	bool prim;
-	int hatar;
-	int counter=0;
-	printf("Határ: ");
-	scanf("%d",&hatar);
-	printf("\n\nprímek:\n\n");
-	for(int i=3; i<hatar; i+=2){
-		prim=true;
-		for(int j=2;j<i;j++){
-			if(i%j==0)
-				prim=false;
+typedef struct elem {
+	int ertek;
+	elem* kovetkezo;
+	elem* elozo;
+}elem_t;
+
+void felszabadit(elem_t* fej){
+	if(fej == NULL){
+		printf("Nem szabadítok fel nullpointert!\n");
+		return;
+	}
+	elem_t* utolso = fej;
+	else{
+		while(utolso->kovetkezo!=NULL)
+			utolso = utolso->kovetkezo;
+		while(utolso != fej){
+			utolso = utolso->elozo;
+			free(utolso->kovetkezo);
 		}
-		if(prim){
+		free(fej);
+		printf("Sikeres felszabadítás!\n\n");
+		return;
+	}
+}
+
+// bemenete egy láncolt lista farka és a hozzáfűzendő elem
+// kimenete a hozzáfűzés utáni farok
+elem_t* hozzafuz(elem_t* farok, int ujErtek){
+	if(farok==NULL){
+		printf("Nem fűzök hozzá nullpointerhez!\n");
+		return NULL;
+	}
+	else{
+		farok->kovetkezo = (elem_t*)malloc(sizeof(elem_t));
+		farok->kovetkezo->elozo = farok;
+		farok->kovetkezo->kovetkezo = NULL;
+		farok = farok->kovetkezo;
+		return farok;
+	}
+}
+
+int main(){
+	bool lehetPrim;
+	int hatar;
+	int counter=1;
+	elem_t* aktualElemPtr;
+	void szemet;
+	
+	// linkelt lista feje és farka, vagyis első és utolsó eleme
+	felem_t* fejPtr, farokPtr;
+       	fejPtr = (elem_t*) malloc(sizeof(elem_t));
+	farokPtr = fejPtr;
+	
+	printf("Határ: "); // felső határérték beolvasása
+	szemet = (void)scanf("%d",&hatar);
+
+	printf("\n\nPrímek %d-ig:\n\n", &hatar); // prímek felsorolása
+	if(hatar > 1)
+		printf("1.  2\n");
+
+	for(int i=3; i<hatar; i+=2){
+		lehetPrim=true;
+		aktualElemPtr = fejPtr;
+		while(aktualElemPtr != NULL && lehetPrim){
+			
+			aktualElemPtr = aktualElemPtr->kovi;
+		}
+		if(lehetPrim){
 			counter++;
 			printf("%d.  %d\n",counter,i);
 		}
 	}
+	// felszabadítás
+	felszabadit(fejPtr);
 	return 0;
 }
