509624bf85cc95a31e46624ea846bce9aa5d9b97
Gibigabcsusz <gbr.szilagyi@gmail.com>
2022-04-14 22:52:32 +0200

    időméréssel teljes, jó pontosan

--- primszuro.c
+++ primszuro.c
@@ -1,39 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <time.h>
 
-typedef struct elem {
+typedef struct elem{
 	int ertek;
-	elem* kovetkezo;
-	elem* elozo;
-}elem_t;
+	struct elem* kovetkezo;
+	struct elem* elozo;
+}elem;
 
-void felszabadit(elem_t* fej){
+void felszabadit(elem* fej){
 	if(fej == NULL){
 		printf("Nem szabadítok fel nullpointert!\n");
 		return;
 	}
-	elem_t* utolso = fej;
 	else{
-		while(utolso->kovetkezo!=NULL)
+		int byteSzam=0;
+		struct elem* utolso = fej;
+		while(utolso->kovetkezo!=NULL){
 			utolso = utolso->kovetkezo;
+			byteSzam += sizeof(elem);
+		}
 		while(utolso != fej){
 			utolso = utolso->elozo;
 			free(utolso->kovetkezo);
 		}
 		free(fej);
-		printf("Sikeres felszabadítás!\n\n");
+		printf("Sikeres felszabadítás: %d B \n",byteSzam);
 		return;
 	}
 }
 
 // bemenete egy láncolt lista farka és a hozzáfűzendő elem
 // kimenete a hozzáfűzés utáni farok
-elem_t* hozzafuz(elem_t* farok, int ujErtek){
-	farok->kovetkezo = (elem_t*)malloc(sizeof(elem_t));
-	farok->kovetkezo->elozo = farok;
-	farok->kovetkezo->kovetkezo = NULL;
-	farok = farok->kovetkezo;
+elem* hozzafuz(struct elem* farok, int ujErtek){
+	if(farok == NULL){
+		farok = (elem*)malloc(sizeof(elem));
+		farok->elozo = NULL;
+		farok->kovetkezo = NULL;
+	}
+	else{
+		farok->kovetkezo = (elem*)malloc(sizeof(elem));
+		farok->kovetkezo->elozo = farok;
+		farok->kovetkezo->kovetkezo = NULL;
+		farok = farok->kovetkezo;
+	}
+	farok->ertek = ujErtek;
 	return farok;
 }
 
@@ -41,20 +53,20 @@ int main(){
 	bool lehetPrim;
 	int hatar;
 	int counter=1;
-	elem_t* aktualElemPtr;
-	void szemet;
-	
+	struct elem* aktualElemPtr;
 	// linkelt lista feje és farka, vagyis első és utolsó eleme
-	felem_t* fejPtr, farokPtr;
-       	fejPtr = hozzafuz((elem_t*)NULL, 2);
+	struct elem *fejPtr, *farokPtr;
+       	fejPtr = hozzafuz(NULL, 2);
 	farokPtr = fejPtr;
 	
 	printf("Határ: "); // felső határérték beolvasása
-	szemet = (void)scanf("%d",&hatar);
+	scanf("%d",&hatar);
 
-	printf("\n\nPrímek %d-ig:\n\n", &hatar); // prímek felsorolása
-
-	for(int i=3; i<hatar; i+=2){
+	clock_t kezdet = clock();
+	printf("\n\nPrímek %d-ig:\n\n", hatar); // prímek felsorolása
+	if(hatar > 1)
+		printf("1.  2\n");
+	for(int i=3; i<=hatar; i+=2){
 		lehetPrim = true;
 		aktualElemPtr = fejPtr;
 		while(aktualElemPtr != NULL && lehetPrim){
@@ -62,7 +74,7 @@ int main(){
 				lehetPrim = false;
 			}
 			else{
-				aktualElemPtr = aktualElemPtr->kovi;
+				aktualElemPtr = aktualElemPtr->kovetkezo;
 			}
 		}
 		// ha prím a vizsgált szám
@@ -73,6 +85,12 @@ int main(){
 		}
 	}
 	// felszabadítás
+	printf("\n");
 	felszabadit(fejPtr);
+	// időmérés
+	clock_t veg = clock();
+	double eltelt = (double)(veg-kezdet)/CLOCKS_PER_SEC;
+	printf("Futási idő: %f másodperc\n", eltelt);
+	
 	return 0;
 }
